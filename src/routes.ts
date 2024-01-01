43a3f238aa684ce4feece18d51e88c4f394ea35d
paulopariz <paulopariz01@gmail.com>
2023-12-31 21:34:22 -0300

    ✨ feat - create

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,26 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
-export async function routes (fastify: FastifyInstance, options: FastifyPluginOptions) {
-    fastify.get('/teste', async(request: FastifyRequest, reply: FastifyReply) => {
-        return { ok: true }
-    })
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
+  fastify.get(
+    "/teste",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return { ok: true };
+    }
+  );
+
+  fastify.post(
+    "/customer",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new CreateCustomerController().handle(request, reply);
+    }
+  );
 }
